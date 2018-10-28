7e766e7f1889d45974e94ad2198a39af914c3390
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2018-10-28 16:59:08 +0900

    tmp commit

--- Programming/Queue/main.c
+++ Programming/Queue/main.c
@@ -1,21 +1,66 @@
 #include <stdio.h>
 
+#define SIZE 10
+
 struct ringBuffer {
-	int Buffer[10];
-	int *head;
-	int *tail;
+	char buffer[SIZE];
+	int head;
+	int tail;
 };
+
+void printQueue(struct ringBuffer *q)
+{
+	int i;
+
+	printf("head[%d], tail[%d]\n", q->head, q->tail);
+	for(i=0; i<SIZE; i++) {
+		printf("%c ", q->buffer[i]);
+	}
+	printf("\n");
+}
+	
+void enqueue(struct ringBuffer *q, int number)
+{
+	char item = '0' + number;
+	if (q->buffer[q->tail] == '_') {
+		q->buffer[q->tail] = item;
+		if (q->tail == 0) 
+			q->tail = SIZE - 1;
+		else
+			q->tail--;
+	}
+}
+
+int dequeue(struct ringBuffer *q)
+{
+	char item;
+	item = q->buffer[q->head];
+	q->buffer[q->head] = '_';
+	if (q->head == 0)
+		q->head = SIZE - 1;
+	else
+		q->head--;
+	return item;
+}
 
 int main(void)
 {
-	struct ringBuffer ringBuffer;
+	struct ringBuffer queue;
 	int i;
-	ringBuffer.head = ringBuffer.Buffer[0];
-	for(i=0; i<10; i++) {
-		ringBuffer.Buffer[i] = i;
-		
-		printf("%d ", ringBuffer.Buffer[i]);
+
+	//initialize queue
+	queue.head = 0;
+	queue.tail = 0;
+	for(i=0; i<SIZE; i++)
+		queue.buffer[i] = '_';
+	printQueue(&queue);
+
+	for (i=0; i<20; i++){
+		enqueue(&queue, i);
+		printQueue(&queue);
 	}
-	printf("\n");
+	printf("dequeue: %c\n", dequeue(&queue));
+	printQueue(&queue);
+	
 	return 0;
 }
