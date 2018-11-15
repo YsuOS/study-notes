60488d0ed74ea2ad64b676f2fdb628acfcdaa3f5
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2018-11-16 00:25:54 +0900

    update List

--- Project/List/main.c
+++ Project/List/main.c
@@ -21,39 +21,88 @@ void printList(node_t *tail)
 		head = head->next;
 		printf("-> %c ", head->value);
 	}
-	printf("\n", head->value);
+	printf("\n");
 	
 
 }
+
 void push(node_t *tail)
 {
 	node_t *head;
 	node_t *new_node = (node_t *)malloc(sizeof(node_t));
-	new_node->value = '0' + makeRand();
 
-	if(tail->next == NULL) {
-		head = tail;	
-	} else {
-		head = tail->next;
-		while(head->next != NULL){
-			head = head->next;
-		}
-	}
-	head->next = new_node;
+	new_node->value = '0' + makeRand();
 	new_node->next = NULL;
+	head = tail;	
+	while(head->next != NULL)
+		head = head->next;
+	head->next = new_node;
 }
-// remove; remove node
+
+void pop(node_t *tail)
+{
+	node_t *head;
+
+	head = tail;
+	if (head->next == NULL)
+		return;
+	while(head->next->next != NULL)
+		head = head->next;
+	printf("pop : %c\n", head->next->value);
+	free(head->next);
+	head->next = NULL;
+}
+// insert; insert a new node to * place from tail(except tail)
+void insert(node_t *tail, int n)
+{
+	node_t *p;
+	node_t *new_node = (node_t *)malloc(sizeof(node_t));
+	if(n <= 0){
+		free(new_node);
+		return;
+	}
+	new_node->value = '0' + makeRand();
+	new_node->next = NULL;
+	p = tail;
+	printf("insert %c to %d place from tail\n", new_node->value, n);
+	while(--n != 0 && p->next != NULL)
+		p = p->next;
+	if(n != 0){
+		free(new_node);
+		return;
+	}
+	new_node->next = p->next;	
+	p->next = new_node;
+
+}
+// remove; remove a node from * place from tail(except tail)
 int main(void)
 {
 	int i;
-	node_t tail;
+	node_t tail, *p;
 	srand(time(NULL));
 	tail.next = NULL;
 
-	for(i=0; i<5; i++)
+	for(i=0; i<5; i++){
 		push(&tail);
-
+		printList(&tail);
+	}
+	for(i=0; i<2; i++){
+		pop(&tail);
+		printList(&tail);
+	}
+	insert(&tail,2);
+	printList(&tail);
+	insert(&tail,4);
+	printList(&tail);
+	insert(&tail,8);
 	printList(&tail);
 
+	//free memory of all nodes
+	p = &tail;
+	while(p->next != NULL){
+		p = p->next;
+		free(p);
+	}
 	return 0;
 }
